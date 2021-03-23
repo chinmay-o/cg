f9adc5a01a46847500cbc677bb41c8f7bba7ba20
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-03-23 18:15:36 +0530

    Add files via upload

---
+++ careeranalytica.github.io/file/index.js
@@ -0,0 +1,58 @@
+
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+$('.carouselMobile').carousel({
+    interval: 2500
+});
+
+$("#Guidance").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#GuidanceContent").offset().top - 120
+      }, 1000);
+});
+
+$("#Counselling").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#CounsellingContent").offset().top - 160
+      }, 1000);
+});
+
+$("#Partners").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#AboutContent").offset().top - 160
+      }, 1000);
+});
+
+$("#TakeTest").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#TakeAssess").offset().top - 160
+      }, 1000);
+});
+
+$(".counselLink #knowMore").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".contactUs").offset().top - 160
+      }, 1000);
+});
+
+$("#takeTestOne").click(function() {
+
+    window.location.href = "https://rzp.io/l/essa";
+});
+
+$("#takeTestTwo").click(function() {
+
+    window.location.href = "https://rzp.io/l/CICSA";
+});
+
+$("#takeTestThree").click(function() {
+
+    window.location.href = "#";
+});
---
+++ careeranalytica.github.io/index.js
@@ -0,0 +1,51 @@
+
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+$('.carouselMobile').carousel({
+    interval: 2500
+});
+
+$("#Guidance").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#GuidanceContent").offset().top - 120
+      }, 1000);
+});
+
+$("#Counselling").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#CounsellingContent").offset().top - 160
+      }, 1000);
+});
+
+$("#Partners").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#AboutContent").offset().top - 160
+      }, 1000);
+});
+
+$(".counselLink #knowMore").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".contactUs").offset().top - 160
+      }, 1000);
+});
+
+$("#takeTestOne").click(function() {
+
+    window.location.href = "https://rzp.io/l/mi0fyHW";
+});
+
+$("#takeTestTwo").click(function() {
+
+    window.location.href = "https://rzp.io/l/rLLou7v";
+});
+
+$("#takeTestThree").click(function() {
+
+    window.location.href = "#";
+});
